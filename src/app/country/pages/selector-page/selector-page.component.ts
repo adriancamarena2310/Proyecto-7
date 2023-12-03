0f102af0696582c31d269faad73a4374d7594c8e
Adrian Camarena <adriancamarena2310mail.com>
2023-12-02 21:09:23 -0600

    vide-266 Selector de regiones

--- src/app/country/pages/selector-page/selector-page.component.ts
+++ src/app/country/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../services/countries.service';
+import { Region } from '../interfaces/country.interfaces';
 
 @Component({
   selector: 'app-selector-page',
@@ -16,8 +18,12 @@ export class SelectorPageComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
+    private countriesService: CountriesService
   ) {}
 
+  get regions(): Region[] {
+    return this.countriesService.regions;
+  }
 
   ngOnInit() {
   }
